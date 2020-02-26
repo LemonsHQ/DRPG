fa0464517f8839e7ca964a85e4bedbb9e94d9562
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-02-26 12:24:15 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const { Client, Attachment, RichEmbed } = require('discord.js');
 global.Discord = require('discord.js');
 global.client = new Discord.Client();
+const config = require('./config.json');
 const xp = require('./code/xpl.js');
 const fs = require("fs");
 let db = JSON.parse(fs.readFileSync("./code/storage/xp.json", "utf8"));
@@ -33,9 +34,40 @@ client.on('ready', () => {
 //xp system
 	
 client.on('message', (message, user) => {
-if (message.author.bot) return;
-let xpAdd= Math.floor(Math.random() * 7)+8;
+if (message.author.bot) return; // ignore bots
 
+    // if the user is not on db add the user and change his values to 0
+    if (!db[message.author.id]) db[message.author.id] = {
+        xp: 0,
+        level: 0
+      };
+    db[message.author.id].xp++;
+    let userInfo = db[message.author.id];
+    if(userInfo.xp > 100) {
+        userInfo.level++
+        userInfo.xp = 0
+        message.reply("Congratulations, you level up")
+    }
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    if(cmd === "info") {
+        let userInfo = db[message.author.id];
+        let member = message.mentions.members.first();
+        let embed = new Discord.RichEmbed()
+        .setColor(0x4286f4)
+        .addField("Level", userInfo.level)
+        .addField("XP", userInfo.xp+"/100");
+        if(!member) return message.channel.sendEmbed(embed)
+        let memberInfo = db[member.id]
+        let embed2 = new Discord.RichEmbed()
+        .setColor(0x4286f4)
+        .addField("Level", memberInfo.level)
+        .addField("XP", memberInfo.xp+"/100")
+        message.channel.sendEmbed(embed2)
+    }
+    fs.writeFile("./database.json", JSON.stringify(db), (x) => {
+        if (x) console.error(x)
+      });
 });
 	
 
