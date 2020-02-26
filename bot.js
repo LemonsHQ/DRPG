106a8b84f8079e8a75cdab4e278d0b5307a2437d
LemonsHQ <51181899+LemonsHQ@users.noreply.github.com>
2020-02-26 12:09:01 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,27 @@
+const { Client, Attachment, RichEmbed } = require('discord.js');
+global.Discord = require('discord.js');
+global.client = new Discord.Client();
 
+
+const xp = require('./coders/xpsystem.js');
+client.setMaxListeners(0)
+if(xp && Client){
+   		 xp.code();
+    		console.log('Timer loaded');
+}
+client.login(process.env.BOT_TOKEN);
+//BOT_TOKEN is the Client Secret
+
+client.once('ready', () => {
+	console.log('Bot intitialized');
+});
+
+client.on('ready', () => {
+    client.user.setStatus('available')
+    client.user.setPresence({
+        game: {
+            name: 'Summons, Type .help',
+	    type: "Listening"
+        }
+    });
+});
